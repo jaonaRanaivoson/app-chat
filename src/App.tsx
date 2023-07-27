b5c09812d41d4eb59a1ccf0f47c4f733ddb95353
jaonaRanaivoson <ranjamick@gmail.com>
2023-07-27 12:35:00 +0300

    First commint

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import Login from './pages/authentification/login';
+
+const App : FunctionComponent  = () => {
+    return (
+        <div>
+            <Login/>
+        </div>
+    );
+} 
+
+export default App;
---
+++ src/components/chat_txt_box.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const ChartTxtBox : FunctionComponent = () => {
+    return (
+        <div>
+            <input type='text' name='txt_code'></input>
+        </div>
+    );
+}
+
+export default ChartTxtBox;
